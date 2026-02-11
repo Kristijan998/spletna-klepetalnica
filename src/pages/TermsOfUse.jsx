3b888ea29bf913621afe37289282978e0be4a303
Kristijan <124184526+Kdnfj@users.noreply.github.com>
2026-02-11 19:45:12 +0100

    Popravek: pravilen import createPageUrl za Vercel/Linux

--- src/pages/TermsOfUse.jsx
+++ src/pages/TermsOfUse.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Button } from "@/components/ui/button";
 import { ArrowLeft } from "lucide-react";
-import { createPageUrl } from "@/utils";
+import { createPageUrl } from "@/utils/index";
 import { t } from "@/components/utils/translations";
 
 export default function TermsOfUse() {
